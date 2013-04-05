09f4bed2b1bf2c0dbf1914433d17a233cf4dab99
Douglas Bielinski <kilraze@gmail.com>
2013-04-05 16:30:24 -0700

    First upload, Tutorial Stuff
    just a few pages into the 3D stuff, loading a 3D triangle and getting it
    to spin... exciting...

---
+++ Game1.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace CraterSix
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Camera camera;
+
+        VertexPositionColor[] verts;
+        VertexBuffer vertexBuffer;
+        BasicEffect effect;
+        Matrix worldTranslation = Matrix.Identity;
+        Matrix worldRotation = Matrix.Identity;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+            // Initialize camera
+            camera = new Camera(this, new Vector3(0, 0, 5),
+            Vector3.Zero, Vector3.Up);
+            Components.Add(camera);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Initialize vertices
+            verts = new VertexPositionColor[3];
+            verts[0] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Blue);
+            verts[1] = new VertexPositionColor(new Vector3(1, -1, 0), Color.Red);
+            verts[2] = new VertexPositionColor(new Vector3(-1, -1, 0), Color.Green);
+
+            // Set vertex data in VertexBuffer
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), verts.Length, BufferUsage.None);
+            vertexBuffer.SetData(verts);
+
+            // Initialize the BasicEffect
+            effect = new BasicEffect(GraphicsDevice);
+
+            // Set cullmode to none <THIS SHOULD BE REMOVED TO IMPROVE PERFORMANCE>
+            RasterizerState rs = new RasterizerState();
+            rs.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rs;
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+            // Translation
+            KeyboardState keyboardState = Keyboard.GetState( );
+            if (keyboardState.IsKeyDown(Keys.Left))
+                worldTranslation *= Matrix.CreateTranslation(-.01f, 0, 0);
+            if (keyboardState.IsKeyDown(Keys.Right))
+                worldTranslation *= Matrix.CreateTranslation(.01f, 0, 0);
+            // Rotation
+            worldRotation *= Matrix.CreateRotationY(MathHelper.PiOver4 / 60);
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            //Set object and camera info
+            effect.World = worldRotation * worldTranslation * worldRotation;
+            effect.View = camera.view;
+            effect.Projection = camera.projection;
+            effect.VertexColorEnabled = true;
+
+            // Begin effect and draw for each pass
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives<VertexPositionColor>
+                (PrimitiveType.TriangleStrip, verts, 0, 1);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
